a13438a745493ad716933de061920d729de92f78
Mick Ruabshkin <mikhail.rubashkin@gmail.com>
2025-10-23 17:06:21 +0200

    add --verbose flag

--- main.py
+++ main.py
@@ -1,34 +1,57 @@
 import os
+import sys
 from dotenv import load_dotenv
 from google import genai
-import sys
+from google.genai import types
 
 
 
 def main():
-    print("Hello from ai-agent!")
     load_dotenv()
+
+    args = sys.argv[1:]
+
+    if not args:
+        print("AI Code Assistant")
+        print("\nUsage: uv main.py \"your prompt here\"")
+        print("Example: uv main.py \"How do I build a calculator app?\"")
+        sys.exit(1)
+
     api_key = os.environ.get("GEMINI_API_KEY")
     client = genai.Client(api_key=api_key)
 
-    model = "gemini-2.0-flash-001"
+    user_prompt = " ".join(filter(lambda arg: arg[:2] != "--", args))
+    flags = list(filter(lambda arg: arg.startswith("--"), args))
+    is_verbose = "--verbose" in flags
 
-    if len(sys.argv) < 2:
-        print("No prompt provided")
-        sys.exit(1)
+    messages = [
+        types.Content(role="user", parts=[types.Part(text=user_prompt)]),
+    ]
 
-    question = sys.argv[1]
 
-    response = client.models.generate_content(model=model, contents=question)
 
+    response = generate_content(client, messages)
+    prompt_tokens = response.usage_metadata.prompt_token_count
+    response_tokens = response.usage_metadata.candidates_token_count
     response_text = response.text
-    prompt_tokens_count = response.usage_metadata.prompt_token_count
-    response_tokens_count = response.usage_metadata.candidates_token_count
 
+    if is_verbose:
+        print(f"User prompt: {user_prompt}")
+        print(f"Prompt tokens: {prompt_tokens}")
+        print(f"Response tokens: {response_tokens}")
+
+    print("Response:")
     print(response_text)
-    print(f"Prompt tokens: {prompt_tokens_count}")
-    print(f"Response tokens: {response_tokens_count}")
 
+
+
+def generate_content(client, messages):
+    response = client.models.generate_content(
+        model="gemini-2.0-flash-001",
+        contents=messages,
+    )
+
+    return response
 
 
 if __name__ == "__main__":
