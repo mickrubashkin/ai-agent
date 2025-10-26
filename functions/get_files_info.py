5f069284fbbf4afc6d017e1f7f9b34e7d6b4dacc
Mick Ruabshkin <mikhail.rubashkin@gmail.com>
2025-10-26 09:41:35 +0200

    add get_files_info function

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -1,23 +1,47 @@
+from genericpath import isfile
 import os
 
 
 def get_files_info(working_directory, directory="."):
-    path = os.path.join(working_directory, directory)
-    work_dir_path = os.path.abspath(working_directory)
-    is_in_workdir = os.path.abspath(path).startswith(work_dir_path)
+    try:
+        path = os.path.join(working_directory, directory)
+        work_dir_path = os.path.abspath(working_directory)
+        is_in_workdir = os.path.abspath(path).startswith(work_dir_path)
 
-    if not is_in_workdir:
-        return f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
+        if not is_in_workdir:
+            return f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
 
-    if not os.path.isdir(path):
-        return f'Error: "{directory}" is not a directory'
+        if not os.path.isdir(path):
+            return f'Error: "{directory}" is not a directory'
 
-    # Build and return a string representing the contents of the directory. It should use this format:
-    # - README.md: file_size=1032 bytes, is_dir=False
-    # - src: file_size=128 bytes, is_dir=True
-    # - package.json: file_size=1234 bytes, is_dir=False
-    print("Path: ", path)
-    print("Path is inside workdir: ", is_in_workdir)
-    print(os.listdir(path))
+        files_info = []
+        dir_files = os.listdir(path)
 
-    return 'hey hey, maybe later'
+        for file_name in dir_files:
+            file_path = os.path.join(path, file_name)
+            is_dir = os.path.isdir(file_path)
+            file_size = calc_size(file_path)
+            file_info = f"- {file_name}: file_size={file_size}bytes, is_dir={is_dir}"
+            files_info.append(file_info)
+
+        return "\n".join(files_info)
+
+    except Exception as e:
+        return f"Error: {e}"
+
+
+def calc_size(path, size=0):
+    if os.path.isfile(path):
+        return os.path.getsize(path)
+
+    total_size = size
+    children = os.listdir(path)
+
+    for child in children:
+        child_path = os.path.join(path, child)
+        if os.path.isfile(child_path):
+            total_size += os.path.getsize(child_path)
+        else:
+            total_size += calc_size(child_path, size=total_size)
+
+    return total_size
--- tests.py
+++ tests.py
@@ -1,24 +1,21 @@
 from functions.get_files_info import get_files_info
 
 def test():
+    print("Result for current directory:")
     print(get_files_info("calculator", "."))
-    # Result for current directory:
-    # - main.py: file_size=719 bytes, is_dir=False
-    # - tests.py: file_size=1331 bytes, is_dir=False
-    # - pkg: file_size=44 bytes, is_dir=True
+    print("")
 
+    print("Result for 'pkg' directory:")
     print(get_files_info("calculator", "pkg"))
-    # Result for 'pkg' directory:
-    # - calculator.py: file_size=1721 bytes, is_dir=False
-    # - render.py: file_size=376 bytes, is_dir=False
+    print("")
 
+    print("Result for '/bin' directory:")
     print(get_files_info("calculator", "/bin"))
-    # Result for '/bin' directory:
-    # Error: Cannot list "/bin" as it is outside the permitted working directory
+    print("")
 
+    print("Result for '../' directory:")
     print(get_files_info("calculator", "../"))
-    # Result for '../' directory:
-    #     Error: Cannot list "../" as it is outside the permitted working directory
+    print("")
 
 
 test()
